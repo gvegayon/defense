e70f0c8b20f62a46819ddf245d7814a1a201ab18
George G. Vega Yon <g.vegayon@gmail.com>
2020-06-20 13:10:50 -0700

    No more slides, now polishing

---
+++ fig/ukfaculty.R
@@ -0,0 +1,21 @@
+library(netplot)
+library(magrittr)
+data(UKfaculty, package="igraphdata")
+
+set.seed(1231)
+graphics.off()
+oldpar <- par(lwd=10)
+svg(file = "ukfaculty-igraph.svg", width = 7, height=5)
+#nplot(UKfaculty, vertex.color = "gray40", edge.width = 2)
+nplot(
+  UKfaculty,
+  vertex.color = "gray40",
+  edge.width = 2,
+  edge.line.breaks=5,
+  edge.color = ~ ego(alpha=.5) + alter,
+  edge.color.alpha=c(.2, .7),
+  edge.color.mix=.9
+)
+dev.off()
+par(oldpar)
+
